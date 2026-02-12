54bba548f552f18e0ef0611b0d8c7c0712021197
jeanlouisrandriamamonjiraibe <107831780+jeanlouisrandriamamonjiraibe@users.noreply.github.com>
2026-02-12 10:45:17 +0300

    API gestion commande

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/boutiques', require('./src/routes/boutique.routes'));
 app.use('/api/produits', require('./src/routes/produit.routes'));
 app.use('/api/stock', require('./src/routes/gestionStock.routes'));
 app.use('/api/categorie', require('./src/routes/categorie.routes'));
-//app.use('/api/commandes', require('./src/routes/commande.routes'));
+app.use('/api/commandes', require('./src/routes/commande.routes'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
